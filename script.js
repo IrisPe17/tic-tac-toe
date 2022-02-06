904f8edb89eec4e7e3c83f2ee26053891e4eb041
Iris Perisa <iris.perisa@gmail.com>
2022-02-06 17:59:43 +0100

    feat: adds pc play

--- script.js
+++ script.js
@@ -34,7 +34,10 @@ const winScenarios = [
   [0, 4, 8], [2, 4, 6],
 ];
 
-let marker = false;
+let marker = false; // X
+let vsPc = false;
+let player = false; // X
+let playerMove = true;
 
 const gameBoard = document.getElementById('board');
 const htmlArray = board.map((field, index) => `<div class="grid-item" id="${field.id}" onclick="play(${index})">&nbsp;</div>`);
@@ -45,13 +48,21 @@ const checkTie = () => {
   if (tie) {
     return alert("It's a tie!!");
   }
+
+  playerMove = !playerMove;
+
+  if (vsPc && !playerMove) {
+    return playPc();
+  }
 };
 
+const getBoardState = () => winScenarios.map((indices) => {
+  const values = indices.map((index) => board[index].value);
+  return values.join('');
+});
+
 const checkWin = () => {
-  const state = winScenarios.map((indices) => {
-    const values = indices.map((index) => board[index].value);
-    return values.join('');
-  });
+  const state = getBoardState();
 
   const xWon = state.some((value) => value === 'XXX');
   const oWon = state.some((value) => value === 'OOO');
@@ -75,10 +86,63 @@ const play = (coordinate) => {
 
   marker = !marker;
 
-  setTimeout(() => checkWin(), 10);
+  return setTimeout(() => checkWin(), 100);
 };
 
-const reset = () => {
+const playPc = () => {
+  // can I win
+  const pc = !player;
+  const winChars = pc ? 'OO' : 'XX';
+  const loseChars = !pc ? 'OO' : 'XX';
+
+  const state = getBoardState();
+  const win = state.indexOf(winChars);
+  if (win > -1) {
+    const indices = winScenarios[win];
+    const index = indices.filter((i) => board[i].value === undefined);
+    setTimeout(() => play(index[0]), 500);
+    return;
+  }
+  // can I lose
+  const lose = state.indexOf(loseChars);
+  if (lose > -1) {
+    const indices = winScenarios[lose];
+    const index = indices.filter((i) => board[i].value === undefined);
+    setTimeout(() => play(index[0]), 500);
+    return;
+  }
+  // play()
+  const possibleFields = board.reduce((availableFields, field, index) => {
+    if (field.value === undefined) {
+      availableFields.push({ ...field, index });
+    }
+    return availableFields;
+  }, []);
+
+  possibleFields.sort(() => Math.random() > 0.5 ? 1 : -1);
+
+  setTimeout(() => play(possibleFields[0].index), 500);
+};
+
+const newGameVsPc = () => {
+  if (!vsPc) {
+    return;
+  }
+
+  const rnd = Math.random();
+  if (rnd >= 0.5) {
+    player = true; // O
+    playerMove = false;
+    alert("PC plays first!");
+    playPc();
+  } else {
+    player = false;
+    playerMove = true;
+    alert("You go first!");
+  }
+};
+
+const reset = (isAgainstPc = false) => {
   const confirmation = confirm('Are you sure?');
   if (confirmation) {
     board.forEach((field) => {
@@ -87,5 +151,10 @@ const reset = () => {
       domField.innerHTML = '&nbsp;';
       marker = false;
     });
+    vsPc = isAgainstPc;
+
+    if (isAgainstPc) {
+      newGameVsPc();
+    }
   }
 };
